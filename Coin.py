81f49b6619c182a037a93a30364599d90d1f29e5
VarunMishra18 <40359771+VarunMishra18@users.noreply.github.com>
2019-09-26 13:48:51 +0530

    Update Coin.py
    A condition is added. If the amount entered by the user is greater than the value of the available denominations of the coin then the program must not execute and show some message.

--- Coin.py
+++ Coin.py
@@ -1,12 +1,21 @@
 from CoinChange import coin_change
 
+flag = False
 
 def main():
-    coins = [1, 2, 5]  # coin denominations
-    amount = 12  # amount of money
+    coins = [5,2,1]  # coin denominations
+    amount = 12  # amo0unt of money
 
     print("No. of ways to form total: {}".format(coin_change(coins, amount)))
 
+    for i in range(len(coins)):
+        if coins[i]>amount:
+            flag = True
 
-if __name__ == "__main__":
-    main()
+
+if flag == True:
+    print("Please enter amount greater than the available denominations..")
+else:
+    if __name__ == "__main__":
+        main()
+
