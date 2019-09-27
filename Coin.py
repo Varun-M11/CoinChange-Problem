a61a82cc784188e157dcf4dddb0ccb9c5f1ed401
VarunMishra18 <40359771+VarunMishra18@users.noreply.github.com>
2019-09-28 00:34:13 +0530

    Update Coin.py
    If amount is Greater than the available denominations then it must not run the algorithm.

--- Coin.py
+++ Coin.py
@@ -1,21 +1,21 @@
 from CoinChange import coin_change
 
-flag = False
-
 def main():
-    coins = [5,2,1]  # coin denominations
-    amount = 12  # amo0unt of money
-
-    print("No. of ways to form total: {}".format(coin_change(coins, amount)))
+    coins = [5,6,7]  # coin denominations
+    amount = 4  # amo0unt of money
+    flag = False
 
     for i in range(len(coins)):
         if coins[i]>amount:
             flag = True
 
+    if flag == True:
+        print("Please enter amount greater than the available denominations..")
+    else:
+        print("No. of ways to form total: {}".format(coin_change(coins, amount)))
 
-if flag == True:
-    print("Please enter amount greater than the available denominations..")
-else:
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
+
 
